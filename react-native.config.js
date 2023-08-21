366d40123b83fb5c5eebb50449fb26157d4862db
Sendipat04 <sendi.patryjar@rey.id>
2023-08-21 12:36:33 +0700

    update code

---
+++ react-native.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    dependencies: {
+        'react-native-flipper': {
+            platforms: {
+                ios: null,
+            },
+        },
+    },
+    project: {
+      ios: {},
+      android: {},
+    },
+    assets: ['./src/assets/fonts'],
+  };
+  
