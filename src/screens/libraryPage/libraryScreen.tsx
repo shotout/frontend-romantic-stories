abb59736f6afffdd652982b334a15e00f2273d28
mobiledev <mobiledev@172-10-30-13.lightspeed.rlghnc.sbcglobal.net>
2023-09-27 14:53:11 +0700

    handle library integrasi

--- src/screens/libraryPage/libraryScreen.tsx
+++ src/screens/libraryPage/libraryScreen.tsx
@@ -18,6 +18,7 @@ import {
   TextInput,
   Alert,
   Pressable,
+  ScrollView,
 } from 'react-native';
 import {bg, cover1, cover2, libraryAdd, logo} from '../../assets/images';
 import {code_color} from '../../utils/colors';
@@ -32,6 +33,7 @@ import BackRightSvg from '../../assets/icons/backRight.jsx';
 import LibraryAddSvg from '../../assets/icons/libraryAdd';
 import ShareSvg from '../../assets/icons/share';
 import DeleteSvg from '../../assets/icons/delete';
+import EditSvg from '../../assets/icons/edit';
 import DotSvg from '../../assets/icons/dot.jsx';
 import PropTypes from 'prop-types';
 import dispatcher from './dispatcher';
@@ -41,31 +43,19 @@ import {SwipeListView} from 'react-native-swipe-list-view';
 import ModalLibrary from '../../components/modal-library';
 import ModalNewLibrary from '../../components/modal-new-library';
 import ModalSorting from '../../components/modal-sorting';
+import {deleteMyCollection, getMyCollection} from '../../shared/request';
+import {BACKEND_URL} from '../../shared/static';
 
 const LibraryScreen = ({colorTheme}) => {
   const [bgTheme, setBgTheme] = useState(colorTheme);
   const [showModal, setShowModal] = useState(false);
   const [showModalNew, setShowModalNew] = useState(false);
   const [showModalSort, setShowModalSort] = useState(false);
+  const [edit, setEdit] = useState(false);
+  const [id, setId] = useState(null);
+  const [listCollection, setListCollection] = useState([]);
   const [listLibrary, setListLibrary] = useState([
-    {
-      name: 'Recently added collection',
-      icon: <LibrarySvg />,
-      iconRight: <BackRightSvg />,
-      type: 1,
-      title: '',
-      desc: '',
-      price: '',
-    },
-    {
-      name: '',
-      icon: <LibrarySvg />,
-      iconRight: <BackRightSvg />,
-      type: 0,
-      title: 'Fistful of Reefer',
-      desc: 'I Miss You',
-      price: 'USD 0,50 For 1 Week Access',
-    },
+   
   ]);
 
   const renderCollect = item => (
@@ -83,57 +73,98 @@ const LibraryScreen = ({colorTheme}) => {
     </View>
   );
 
-  const renderContent = item => (
-    <View>
-      <View
-        style={{
-          paddingHorizontal: 10,
-          paddingBottom: 10,
-          flexDirection: 'row',
-          alignItems: 'center',
-          backgroundColor: bgTheme,
-          borderColor: '#778DFF',
-          borderTopWidth: 1,
-          borderBottomWidth: 1,
-          paddingVertical: 10,
-        }}>
-        <Image source={cover1} />
+  const renderContent = item => {
+    return (
+      <View>
         <View
           style={{
-            marginLeft: 20,
-            flex: 1,
-            justifyContent: 'center',
-            alignContent: 'center',
+            paddingHorizontal: 10,
+            paddingBottom: 10,
+            flexDirection: 'row',
+            alignItems: 'center',
+            backgroundColor: bgTheme,
+            borderColor: '#778DFF',
+            borderTopWidth: 1,
+            borderBottomWidth: 1,
+            paddingVertical: 10,
           }}>
-          <Text allowFontScaling={false} style={{color: code_color.white}}>
-            Fistful of Reefer
-          </Text>
-          <Text allowFontScaling={false} style={{color: code_color.white}}>
-            I Miss You
-          </Text>
+          <Image
+            source={{uri: `${BACKEND_URL}${item?.item?.category?.cover?.url}`}}
+            style={{width: 100, height: 100}}
+            resizeMode="contain"
+          />
           <View
             style={{
-              backgroundColor: '#ED5267',
-              padding: 5,
-              borderRadius: 10,
-              marginVertical: 5,
-              width: 150,
+              marginLeft: 20,
+              flex: 1,
+              justifyContent: 'center',
+              alignContent: 'center',
+            }}>
+            <Text allowFontScaling={false} style={{color: code_color.white}}>
+              {item?.item?.title_en}
+            </Text>
+            <Text allowFontScaling={false} style={{color: code_color.white}}>
+              {item?.item?.content_en}
+            </Text>
+            <View
+              style={{
+                backgroundColor: '#ED5267',
+                padding: 5,
+                borderRadius: 10,
+                marginVertical: 5,
+                width: 150,
+              }}>
+              <Text
+                allowFontScaling={false}
+                style={{color: code_color.white, fontSize: 10}}>
+                USD 0,50 For 1 Week Access
+              </Text>
+            </View>
+          </View>
+
+          <DotSvg />
+        </View>
+        {/* <View
+        style={{borderColor: '#778DFF', borderWidth: 1, paddingVertical: 10, backgroundColor: bgTheme}}
+      /> */}
+      </View>
+    );
+  };
+  const renderContentCollection = item => {
+    return (
+      <View>
+        <View
+          style={{
+            paddingHorizontal: 10,
+            paddingBottom: 10,
+            flexDirection: 'row',
+            alignItems: 'center',
+            backgroundColor: bgTheme,
+            borderColor: '#778DFF',
+            borderTopWidth: 1,
+            borderBottomWidth: 1,
+            paddingVertical: 10,
+          }}>
+          <LibrarySvg fill={'white'} width={30} height={30} />
+          <View
+            style={{
+              marginLeft: 20,
+              flex: 1,
+              justifyContent: 'center',
+              alignContent: 'center',
             }}>
             <Text
               allowFontScaling={false}
-              style={{color: code_color.white, fontSize: 10}}>
-              USD 0,50 For 1 Week Access
+              style={{color: code_color.white, paddingVertical: 15}}>
+              {item?.item?.name}
             </Text>
           </View>
-        </View>
 
-        <DotSvg />
+          <BackRightSvg />
+        </View>
       </View>
-      {/* <View
-        style={{borderColor: '#778DFF', borderWidth: 1, paddingVertical: 10, backgroundColor: bgTheme}}
-      /> */}
-    </View>
-  );
+    );
+  };
 
   const closeRow = (rowMap, rowKey) => {
     // if (rowMap[rowKey]) {
@@ -152,12 +183,47 @@ const LibraryScreen = ({colorTheme}) => {
     // newData[section].data.splice(prevIndex, 1);
     // setListData(newData);
   };
+
+  const deleteRowCollection =  async (rowMap) => {
+    console.log(JSON.stringify(rowMap.item.id))
+    try {
+      const res = await deleteMyCollection(rowMap.item.id)
+      handleRestart()
+    } catch (error) {
+      
+    }
+  };
+  const handleEditCollect = (rowMap) => {
+    console.log(JSON.stringify(rowMap.item.id))
+    setId(rowMap.item)
+    setShowModalNew(true)
+    setEdit(true)
+  }
+  useEffect(() => {
+    handleRestart();
+  }, []);
+  const handleRestart = async () => {
+    setShowModalNew(false);
+    try {
+      const res = await getMyCollection();
+      setListCollection(res.data);
+      setListLibrary(res.outsides);
+    } catch (error) {
+      console.log(JSON.stringify(error))
+    }
+  };
   return (
     <View style={{flex: 0, height: 500, backgroundColor: bgTheme}}>
       <ModalLibrary isVisible={showModal} onClose={() => setShowModal(false)} />
       <ModalNewLibrary
         isVisible={showModalNew}
         onClose={() => setShowModalNew(false)}
+        restart={() => {
+          setEdit(false)
+          handleRestart();
+        }}
+        edit={edit}
+        data={id}
       />
       <ModalSorting
         isVisible={showModalSort}
@@ -193,9 +259,47 @@ const LibraryScreen = ({colorTheme}) => {
           />
         </View>
         <Pressable onPress={() => setShowModalSort(true)}>
-        <DescendingSvg fill={code_color.white} />
+          <DescendingSvg fill={code_color.white} />
         </Pressable>
       </View>
+      <ScrollView>
+      <SwipeListView
+        data={listCollection}
+        renderItem={item => renderContentCollection(item)}
+        renderHiddenItem={(_data, _rowMap) => (
+          <View style={styles.rowBack}>
+            <TouchableOpacity
+              style={[styles.backLeftCollectBtn, styles.backLeftBtnCollect]}
+              onPress={() => handleEditCollect(_data)}>
+              <EditSvg />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.backRightCollectBtn, styles.backRightBtnCollect]}
+              onPress={() => {
+                Alert.alert(
+                  'Are you sure you want to remove this collection?',
+                  '',
+                  [
+                    {
+                      text: 'Yes',
+                      onPress: () => {
+                        deleteRowCollection(_data)
+                        // handleDelete(item.id);
+                      },
+                    },
+                    {text: 'Cancel', onPress: () => {}},
+                  ],
+                );
+              }}>
+              <DeleteSvg />
+            </TouchableOpacity>
+          </View>
+        )}
+        rightOpenValue={-120}
+        previewRowKey={'0'}
+        previewOpenValue={-40}
+        previewOpenDelay={3000}
+      />
       <SwipeListView
         data={listLibrary}
         renderItem={item => renderContent(item)}
@@ -208,7 +312,7 @@ const LibraryScreen = ({colorTheme}) => {
             </TouchableOpacity>
             <TouchableOpacity
               style={[styles.backRightBtn, styles.backRightCenter]}
-              onPress={() => deleteRow(_rowMap, _data.name)}>
+              onPress={() => deleteRow(_rowMap, _data.id)}>
               <ShareSvg />
             </TouchableOpacity>
             <TouchableOpacity
@@ -237,6 +341,8 @@ const LibraryScreen = ({colorTheme}) => {
         previewOpenValue={-40}
         previewOpenDelay={3000}
       />
+      </ScrollView>
+     
     </View>
   );
 };
@@ -250,6 +356,24 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     paddingLeft: 15,
   },
+  backLeftCollectBtn: {
+    alignItems: 'center',
+    bottom: 0,
+    justifyContent: 'center',
+    position: 'absolute',
+    top: 0,
+    width: 60,
+    height: 69
+  },
+  backRightCollectBtn: {
+    alignItems: 'center',
+    bottom: 0,
+    justifyContent: 'center',
+    position: 'absolute',
+    top: 0,
+    width: 60,
+    height: 69
+  },
   backRightBtn: {
     alignItems: 'center',
     bottom: 0,
@@ -258,6 +382,14 @@ const styles = StyleSheet.create({
     top: 0,
     width: 60,
     // height: 40
+  },
+  backLeftBtnCollect: {
+    backgroundColor: '#FF932F',
+    right: 60,
+  },
+  backRightBtnCollect: {
+    backgroundColor: '#FF453B',
+    right: 0,
   },
   backRightBtnLeft: {
     backgroundColor: '#797BFE',
