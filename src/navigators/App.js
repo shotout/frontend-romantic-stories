847e30c1a7967d8ca68060586a19e35c5cd732d7
sendiptjr <sendiptjr@gmail.com>
2023-12-30 11:25:40 +0700

    fix: clear notif

--- src/navigators/App.js
+++ src/navigators/App.js
@@ -1,22 +1,45 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/lib/integration/react';
-import {LogBox, Platform, StatusBar} from 'react-native';
+import {AppState, LogBox} from 'react-native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import notifee from '@notifee/react-native';
 
 import Main from './Main';
 import store, {persistor} from '../store/configure-store';
-import { PaperProvider } from 'react-native-paper';
+import {PaperProvider} from 'react-native-paper';
 
 LogBox.ignoreAllLogs();
 
 const App = () => {
+  const [appState, setAppState] = useState(AppState.currentState);
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', nextAppState => {
+      setAppState(nextAppState);
+    });
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (appState === 'active') {
+      const clearNotifications = async () => {
+        await notifee.cancelAllNotifications();
+        await notifee.setBadgeCount(0);
+      };
+      clearNotifications();
+    }
+  }, [appState]);
+
   return (
     <GestureHandlerRootView style={{flex: 1}}>
       <Provider store={store}>
         <PersistGate persistor={persistor}>
-        <PaperProvider>
-          <Main />
+          <PaperProvider>
+            <Main />
           </PaperProvider>
         </PersistGate>
       </Provider>
